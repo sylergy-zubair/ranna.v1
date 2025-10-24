9280d7df46e2d32477c91278b45971cdd6501b14
Abu Zubair <studio.zubair@gmail.com>
2025-10-24 20:43:02 +0600

    config fixed

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pub-1df565ef59df448a9e0f5e4226f6265a.r2.dev',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
